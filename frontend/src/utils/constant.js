8191ec369dd4b8df8135ac4081c50374218c2f41
BHARAT KUMAR <bharatkumarch1122@gmail.com>
2025-12-31 02:44:53 +0530

    Configure API base URL for deployment

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
-export const USER_API_ENDPOINT = "http://localhost:3000/api/v1/user"
-export const JOB_API_END_POINT = "http://localhost:3000/api/v1/jobs"
-export const APPLICATION_API_END_POINT = "http://localhost:3000/api/v1/applications"
-export const COMPANY_API_END_POINT = "http://localhost:3000/api/v1/company"
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const USER_API_ENDPOINT = `${BASE_URL}/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/jobs`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/applications`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
